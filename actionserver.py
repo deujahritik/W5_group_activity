90e367e0e76ece77cc4658e26a09794ade42cc81
Ritik Deuja <92029196+deujahritik@users.noreply.github.com>
2023-03-28 23:53:41 +0900

    Update actionserver.py

--- actionserver.py
+++ actionserver.py
@@ -1,35 +1,34 @@
-# Define the action and service messages:
-Creating `.msg` files in the msg folder of our package to define the structure of the
-action goal, result, and feedback messages, as well as the service request and response messages. 
-Therefor,I created a `PlantStatus.msg` file to define the message structure for plant status information.
-
-## Implementing  service: 
-Now, Writing service code to handle requests for plant status information.
-This might involve reading sensor data or other information from the hardware and returning it as a response.
-
-Here's an example of a simple service implementation:
-
+Implement the action:
+    First writing the action code to handle requests for plant watering. 
+    This might involve activating a water pump or other hardware to water the plant.
+    
+ Here's an example of a simple action implementation:
 import rospy
-from my_package.srv import PlantStatus
+import actionlib
+from my_package.msg import WaterPlantAction, WaterPlantGoal, WaterPlantResult, WaterPlantFeedback
 
-def handle_plant_status(req):
-    # Read sensor data or other information from the hardware
-    temperature = 25.0
-    humidity = 50.0
-    light_intensity = 1000
-
-    # Create the service response
-    status = PlantStatusResponse()
-    status.temperature = temperature
-    status.humidity = humidity
-    status.light_intensity = light_intensity
-
-    return status
-
-def plant_status_server():
-    rospy.init_node('plant_status_server')
-    s = rospy.Service('plant_status', PlantStatus, handle_plant_status)
+def water_plant_server():
+    rospy.init_node('water_plant_server')
+    server = actionlib.SimpleActionServer('water_plant', WaterPlantAction, execute_cb=handle_water_plant, auto_start=False)
+    server.start()
     rospy.spin()
 
+def handle_water_plant(goal):
+    # Activate water pump or other hardware to water the plant
+    watering_time = 5.0 # seconds
+    for i in range(int(watering_time)):
+        # Publish feedback to show progress
+        feedback = WaterPlantFeedback()
+        feedback.percent_complete = float(i + 1) / watering_time * 100.0
+        server.publish_feedback(feedback)
+        rospy.sleep(1.0)
+
+    # Create the action result
+    result = WaterPlantResult()
+    result.success = True
+
+    # Set the action result and finish the action
+    server.set_succeeded(result)
+
 if __name__ == '__main__':
-    plant_status_server()
+    water_plant_server()
