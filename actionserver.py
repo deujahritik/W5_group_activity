c590ad38feebd09956cfbdf726071c8c1bd0164b
Ritik Deuja <92029196+deujahritik@users.noreply.github.com>
2023-03-28 23:46:38 +0900

    Update actionserver.py

--- actionserver.py
+++ actionserver.py
@@ -1,9 +1,13 @@
 # Define the action and service messages:
-*Creating `.msg` files in the msg folder of our package to define the structure of the action goal, result, and feedback messages, as well as the service request and response messages. *
- *created a `PlantStatus.msg` file to define the message structure for plant status information.*
+Creating `.msg` files in the msg folder of our package to define the structure of the
+action goal, result, and feedback messages, as well as the service request and response messages. 
+Therefor,I created a `PlantStatus.msg` file to define the message structure for plant status information.
 
 ## Implementing  service: 
-*Write the service code to handle requests for plant status information. This might involve reading sensor data or other information from the hardware and returning it as a response. Here's an example of a simple service implementation:*
+Now, Writing service code to handle requests for plant status information.
+This might involve reading sensor data or other information from the hardware and returning it as a response.
+
+Here's an example of a simple service implementation:
 
 import rospy
 from my_package.srv import PlantStatus
