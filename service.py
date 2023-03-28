fa543f10ccc446466e518f8b2f0f9e58cdea41e5
Ritik Deuja <92029196+deujahritik@users.noreply.github.com>
2023-03-28 23:37:10 +0900

    Update service.py

--- service.py
+++ service.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
+
+import rospy
+from your_package.srv import YourService, YourServiceResponse
+
+def handle_your_service(req):
+    # handle the service request
+    rospy.loginfo("Handling service request...")
+    your_response_variable = "Response"
+    return YourServiceResponse(your_response_variable)
+
+if __name__ == '__main__':
+    rospy.init_node('your_service_server')
+    # create the service
+    service = rospy.Service('your_service_name', YourService, handle_your_service)
+    rospy.loginfo("Service ready.")
+    rospy.spin()
 
